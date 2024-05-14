c0140cc796ed75eb824372520b3e854d873472b9
Zachary Cherney <zach@Zacharys-MacBook-Pro.local>
2024-05-13 22:00:56 -0500

    Update templates and URLs

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,1 +1,7 @@
-from __future__ import absolute_import, unicode_liter
+from __future__ import absolute_import, unicode_literals
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
+
+default_app_config = 'myproject.apps.MyProjectConfig'
